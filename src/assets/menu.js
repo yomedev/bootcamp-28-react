498a78011e3310028c88d88cd03ef0a96108d535
Yaroslav <yomelianenkowork@gmail.com>
2022-10-04 13:41:53 +0300

    module 1 lesson 1

---
+++ src/assets/menu.js
@@ -0,0 +1,9 @@
+export const menuConfig = [
+  {
+    name: "Университет",
+  },
+
+  {
+    name: "Факультеты",
+  },
+];
---
+++ src/components/image/Image.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Image = ({ src }) => {
+  return (
+    <img src={src} alt="image" width="200px" />
+  )
+}
+
+export default Image
